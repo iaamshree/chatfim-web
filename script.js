9e40a378293993065b3e314cdbc146fab61543cf
iaamshree <shreyashpisal555@gmail.com>
2026-02-25 16:35:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("JS is working");
+function sendMessage() {
+    var message = document.getElementById("message").value;
+
+    if (message.trim() === "") {
+        alert("Please type something!");
+        return;
+    }
+
+    document.getElementById("output").innerText = message;
+
+    document.getElementById("message").value = "";
+}
